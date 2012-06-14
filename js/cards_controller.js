8f0b13db29597accd9a8cb22e84dceb4adec1505
saksmlz <saksmlz@gmail.com>
2012-06-15 02:22:50 +0300

    controller fixed

--- js/cards_controller.js
+++ js/cards_controller.js
@@ -1,40 +1,45 @@
-(function(globalNamespace) {
-  var TaskBoard           = globalNamespace.TaskBoard, CardController,
+$(function() {
+  var TaskBoard           = window.TaskBoard, CardController,
     nameValidationMessage = $('#name_validation_message'),
     newCardPopup          = $('#new_card_popup'),
-    renderCard            = Handlebars.compile($("#card-template").html());
+    renderCard            = Handlebars.compile($("#card-template").html()),
+    Card                  = TaskBoard.Card;
 
-  CardsController = TaskBoard.CardsController = {
 
-    addNewCard: function() {
-      var controlsGroup = newCardPopup.find('.control-group.name');
+  function addNewCard() {
+    var controlsGroup = newCardPopup.find('.control-group.name');
 
-      if ('' === newCardPopup.find('form')[0].name.value) {
-        nameValidationMessage.show();
-        controlsGroup.addClass('error');
-      } else {
-        nameValidationMessage.hide();
-        controlsGroup.removeClass('error');
-        createCard();
-      }
-    },
+    if ('' === newCardPopup.find('form')[0].name.value) {
+      nameValidationMessage.show();
+      controlsGroup.addClass('error');
+    } else {
+      nameValidationMessage.hide();
+      controlsGroup.removeClass('error');
+      createCard();
+    }
+  }
 
-    appendCardToDom: function(card) {
-      var html = $(renderCard(card));
-      html.data('card', card);
-      $('.column.todo').append(html);
-    },
+  function appendCardToDom(card) {
+    var html = $(renderCard(card));
+    html.data('card', card);
+    $('.column.todo').append(html);
+  }
 
-    createCard: function() {
-      var data = newCardPopup.find('form').serializeArray(),
+  function createCard() {
+    var data = newCardPopup.find('form').serializeArray(),
         card = new Card(data[0].value, data[1].value);
 
-      Card.store(card);
-      appendCardToDom(card);
+    Card.store(card);
+    appendCardToDom(card);
 
-      newCardPopup.find('form')[0].reset();
-      newCardPopup.modal('hide');
-    },
+    newCardPopup.find('form')[0].reset();
+    newCardPopup.modal('hide');
+  }
+
+  CardsController = TaskBoard.CardsController = {
+    addNewCard:      addNewCard,
+    appendCardToDom: appendCardToDom,
+    createCard:      createCard,
   };
 
-})(window);
+});
